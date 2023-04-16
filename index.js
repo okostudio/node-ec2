3cffdae78be201ee06c6c8556fae6c450064c7ea
Marek Suchanski <marek@mareks-MacBook-Pro.local>
2023-04-16 19:30:53 +1000

    port changed to 3001

--- index.js
+++ index.js
@@ -86,6 +86,6 @@ app.get('*', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+app.listen(3001, () => {
+  console.log('Server listening on port 3001');
 });
